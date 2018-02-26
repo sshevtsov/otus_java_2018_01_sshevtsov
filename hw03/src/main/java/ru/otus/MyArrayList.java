b1359725a9120f66244360ab9e6f1cdf2066dd92
sshevtsov <shevtsov.sergey31@gmail.com>
2018-02-26 16:54:33 +0300

    hw03

---
+++ hw03/src/main/java/ru/otus/MyArrayList.java
@@ -0,0 +1,208 @@
+package ru.otus;
+
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+public class MyArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final float CAPACITY_INCREASER = 1.33f;
+
+    private Object[] elementData;
+    private int size;
+
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity >= 0) {
+            this.elementData = new Object[initialCapacity > 0 ? initialCapacity : DEFAULT_CAPACITY];
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: "+
+                    initialCapacity);
+        }
+    }
+
+    public MyArrayList() {
+        this.elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<T> operator) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public void sort(Comparator<? super T> c) {
+        Arrays.sort((T[]) elementData, 0, size, c);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] copyArray = new Object[size];
+        System.arraycopy(elementData, 0, copyArray, 0, size);
+        return copyArray;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public boolean add(T t) {
+        if (elementData.length == size) {
+            increaseCapacity();
+        }
+
+        elementData[size] = t;
+        size++;
+
+        return true;
+    }
+
+    private void increaseCapacity() {
+        Object[] incremented = new Object[elementData.length + getSizeToAdd()];
+        System.arraycopy(elementData, 0, incremented, 0, elementData.length);
+        elementData = incremented;
+    }
+
+    private int getSizeToAdd() {
+        return (int) (size * CAPACITY_INCREASER);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elementData[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        checkIndex(index);
+        return (T) elementData[index];
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+size;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T set(int index, T element) {
+        checkIndex(index);
+        T oldValue = (T) elementData[index];
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public T remove(int index) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elementData[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        throw new RuntimeException("the method not implemented");
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return listIterator(0);
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException("Index: "+index);
+        return new MyListIterator<>(this, index);
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        throw new RuntimeException("the method not implemented");
+    }
+}
---
+++ hw03/src/test/java/ru/otus/MyArrayListTest.java
@@ -0,0 +1,87 @@
+package ru.otus;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyArrayListTest {
+    List<Integer> myArrayList;
+
+    @Before
+    public void beforeTest() {
+        myArrayList = new MyArrayList<>();
+    }
+
+    @After
+    public void clearList() {
+        myArrayList.clear();
+    }
+
+    @Test
+    public void testAddAll() {
+        Collections.addAll(myArrayList, 1, 2, 3);
+
+        checkList(myArrayList);
+    }
+
+    @Test
+    public void testCopyTo() {
+        List<Integer> src = new MyArrayList<>();
+        Collections.addAll(src, 1, 2, 3);
+        Collections.addAll(myArrayList, 10, 11, 12);
+        Collections.copy(myArrayList, src);
+        checkList(myArrayList);
+    }
+
+    @Test
+    public void testCopyFrom() {
+        Collections.addAll(myArrayList, 1, 2, 3);
+        List<Integer> dest = new MyArrayList<>();
+        Collections.addAll(dest, 10, 11, 12);
+        Collections.copy(dest, myArrayList);
+        checkList(dest);
+    }
+
+    private void checkList(List<Integer> list) {
+        assertEquals(3, list.size());
+        assertTrue(list.contains(1));
+        assertTrue(list.contains(2));
+        assertTrue(list.contains(3));
+        assertTrue(!list.contains(10));
+        assertTrue(!list.contains(11));
+        assertTrue(!list.contains(12));
+    }
+
+    @Test
+    public void testCopyMoreThenThreshold() {
+        List<Integer> src = new MyArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            src.add(i);
+        }
+        for (int i = 10; i > 0; i--) {
+            myArrayList.add(i);
+        }
+        Collections.copy(myArrayList, src);
+        checkLongList(myArrayList);
+    }
+
+    private void checkLongList(List<Integer> list) {
+        assertEquals(10, list.size());
+        for (int i = 0; i < 10; i++) {
+            assertEquals(i, (int) list.get(i));
+        }
+    }
+
+    @Test
+    public void testSort() {
+        Collections.addAll(myArrayList, 3, 2, 1, 25, 44, 9);
+        Collections.sort(myArrayList);
+        assertArrayEquals(new Integer[] {1, 2, 3, 9, 25, 44}, myArrayList.toArray());
+    }
+
+}
